ddad04c6f1a5b05fc8e79a30766b16cf97171045
Omar AbdelFattah <omarabdelfattah01@gmail.com>
2023-01-15 14:38:48 +0200

    Unit Test End-to-End for Job.

--- Spring-Boot/multiple-data-sources/src/test/java/com/javadevjournal/MultipleDataSourcesCustomerTests.java
+++ Spring-Boot/multiple-data-sources/src/test/java/com/javadevjournal/MultipleDataSourcesCustomerTests.java
@@ -1,38 +1,38 @@
-//package com.javadevjournal;
-//
-//import com.javadevjournal.customer.config.CustomerConfig;
-//import com.javadevjournal.customer.data.CustomerModel;
-//import com.javadevjournal.customer.repo.CustomerRepository;
-//import com.javadevjournal.product.config.ProductConfig;
-//import com.javadevjournal.product.data.ProductModel;
-//import com.javadevjournal.product.repo.ProductRepository;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import static org.junit.Assert.*;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.dao.DataIntegrityViolationException;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class MultipleDataSourcesCustomerTests {
-//
-//
-//    @Autowired
-//    private CustomerRepository customerRepository;
-//
-//    @Test
-//    @Transactional("customerTransactionManager")
-//    public void create_check_customer() {
-//
-//        CustomerModel customer = new CustomerModel("user@javadevjournal.com","Robert","Hickle");
-//        customer = customerRepository.save(customer);
-//
-//        assertNotNull(customerRepository.findById(customer.getId()));
-//        assertEquals(customerRepository.findById(customer.getId()).get().getEmail() ,"user@javadevjournal.com");
-//    }
-//}
+package com.javadevjournal;
+
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ContextConfiguration;
+import com.javadevjournal.mainSource.data.MainSourceModel;
+import com.javadevjournal.mainSource.repo.MainSourceRepository;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.annotations.Test;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultipleDataSourcesCustomerTests {
+
+
+    @Autowired
+    private MainSourceRepository mainSourceRepository;
+
+    @Test
+    @Transactional("mysqlTransactionManager")
+    public void create_check_row() {
+
+        MainSourceModel mainSourceModel = new MainSourceModel(999,999,999,999);
+        mainSourceModel = mainSourceRepository.save(mainSourceModel);
+
+        assertNotNull(mainSourceRepository.findById(mainSourceModel.getId()));
+        assertEquals(mainSourceRepository.findById(mainSourceModel.getId()).get().getTime_stamp() ,"999");
+    }
+
+
+}
---
+++ Spring-Boot/multiple-data-sources/src/test/java/com/javadevjournal/SpringBatchConfigurationTest.java
@@ -0,0 +1,114 @@
+package com.javadevjournal;
+
+import com.javadevjournal.batch.config.BatchConfig;
+import com.javadevjournal.mainSource.data.MainSourceModel;
+import com.javadevjournal.mainSource.repo.MainSourceRepository;
+import jakarta.annotation.Resource;
+import jakarta.transaction.Transactional;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+import javax.sql.DataSource;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBatchTest
+@EnableAutoConfiguration
+//@ContextConfiguration(classes = {BatchConfig.class})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@Transactional
+@ContextConfiguration
+public class SpringBatchConfigurationTest {
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+    @Mock
+    Job job = new Job() {
+        @Override
+        public String getName() {
+            return "Job-";
+        }
+
+        @Override
+        public void execute(JobExecution jobExecution) {
+
+        }
+    };
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Test
+    public void testJob() throws Exception {
+        this.jobLauncherTestUtils.setJob(job);
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+        if( jobExecution.getStatus() == BatchStatus.COMPLETED ){
+
+            jobExecution.setExitStatus(new ExitStatus("COMPLETED"));
+
+        } else if(jobExecution.getStatus() == BatchStatus.FAILED){
+
+            jobExecution.setExitStatus(new ExitStatus("FAILED"));
+
+        }
+        Assert.assertEquals("Job-", jobExecution.getJobInstance().getJobName());
+//        Assert.assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
+    }
+
+//    public JobParameters defaultJobParameters() {
+//        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+//
+//        JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+//        jobParametersBuilder.addString("time", format.format(Calendar.getInstance().getTime())).toJobParameters();
+//
+//        return jobParametersBuilder.toJobParameters();
+//    }
+
+//    @Test
+//    public void givenRefOutput_whenJobExecutes_thenSuccess() throws Exception {
+//        MainSourceModel expectedMainSourceModel = new MainSourceModel(999,999,999,999);
+//        MainSourceModel actualMainSourceModel = new MainSourceModel();
+//
+//        JobExecution jobExecution = jobLauncherTestUtils.launchJob(defaultJobParameters());
+//        JobInstance jobInstance = jobExecution.getJobInstance();
+//        ExitStatus exitStatus = jobExecution.getExitStatus();
+//        Job job = batchConfig.createJob();
+//
+//
+//        assertArrayEquals(jobInstance.getJobName().toCharArray(), "Job-".toCharArray());
+//        assertArrayEquals(exitStatus.getExitCode().toCharArray(), "COMPLETED".toCharArray());
+//
+//    }
+}
